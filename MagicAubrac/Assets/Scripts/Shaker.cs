0874f5f613d610599726643abfaa979405942788
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-10-17 10:09:01 +0200

    [ADD] joycon link and shakerWIP

--- MagicAubrac/Assets/Scripts/Shaker.cs
+++ MagicAubrac/Assets/Scripts/Shaker.cs
@@ -6,12 +6,15 @@ public class Shaker : MonoBehaviour
 {
     [SerializeField] private IngredientType[] _cocktail = new IngredientType[5];
     [SerializeField] private Rune[] _runes = new Rune[3];
+    ClientsManager clients;
+    private bool[] shakenAtStep=new bool[5];
     int currentLayerCocktail;
     int currentLayerRune;
     private void Start()
     {
         currentLayerRune = 0;
         currentLayerCocktail = 0;
+        clients = GameManager.ClientsManager;
     }
     // je n'ai fait que le remplissage pour des raisons de j'en avais besoins il faut completer le tout plus tard :)
 
@@ -28,6 +31,10 @@ public class Shaker : MonoBehaviour
             _cocktail[i] = IngredientType.INVALID;
         }
         currentLayerCocktail = 0;
+        for (int i =0;i< shakenAtStep.Length;i++) 
+        {
+            shakenAtStep[i] = false;
+        }
     }
     public void AddToShaker(Rune rune)
     {
@@ -43,5 +50,17 @@ public class Shaker : MonoBehaviour
         }
         currentLayerRune = 0;
     }
+    public void Shake()
+    {
+        if (currentLayerCocktail != 0)
+        {
+            shakenAtStep[currentLayerCocktail - 1]=true;
+        }
+    }
 
+    public bool CompareReceipe()
+    {
+        
+        return false;
+    }
 }
