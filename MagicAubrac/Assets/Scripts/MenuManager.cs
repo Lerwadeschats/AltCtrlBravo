285f4935139c7175ed1c031d6f739f3f0314569c
Chloe-Ms <mllechloe.mathias@gmail.com>
2024-10-24 12:47:17 +0200

    [ADD] score in menu ★⌒ Ｙ⌒Ｙ⌒Ｙ⌒Ｙ⌒ ヾ(oﾟДﾟ)ﾉ

--- MagicAubrac/Assets/Scripts/MenuManager.cs
+++ MagicAubrac/Assets/Scripts/MenuManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _uiMenu;
+    [SerializeField] private Shaker _shaker;
+    [SerializeField] private TextMeshProUGUI _scoreText;
     public bool IsInMenu { get; set; } = false;
 
     private void Awake()
@@ -21,6 +24,10 @@ public class MenuManager : MonoBehaviour
     private void OnLose()
     {
         IsInMenu = true;
+        if (_scoreText != null)
+        {
+            _scoreText.text = _shaker?.CompletedFull.ToString();
+        }
         _uiMenu?.SetActive(true);
     }
 }
